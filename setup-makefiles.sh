0ba524923cad5924e1c5d3f9c6e8e4bb84984706
jerpelea <jerpelea@gmail.com>
2011-08-29 23:54:30 +0300

    msm7x30: moved adreno libs to vendor
    Change-Id: I7c21694d9805f807b6120714f1e137523b7f0b54

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -168,6 +168,14 @@ vendor/__VENDOR__/__DEVICE__/proprietary/touch_anzu_sharp_type2.hex:system/etc/f
 vendor/__VENDOR__/__DEVICE__/proprietary/touch_anzu_sony_type1.hex:system/etc/firmware/touch_anzu_sony_type1.hex \\
 vendor/__VENDOR__/__DEVICE__/proprietary/touch_anzu_sony_type2.hex:system/etc/firmware/touch_anzu_sony_type2.hex 
 
+## Adreno 200 files
+PRODUCT_COPY_FILES += \\
+vendor/__VENDOR__/__DEVICE__/proprietary/libgsl.so:system/lib/libgsl.so \\
+vendor/__VENDOR__/__DEVICE__/proprietary/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \\
+vendor/__VENDOR__/__DEVICE__/proprietary/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \\
+vendor/__VENDOR__/__DEVICE__/proprietary/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \\
+vendor/__VENDOR__/__DEVICE__/proprietary/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so
+
 
 EOF
 
