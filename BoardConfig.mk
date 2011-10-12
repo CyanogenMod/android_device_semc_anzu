5b2e7d97d98a71386a2535eb2807e9866fa15847
Diogo Ferreira <defer@cyanogenmod.com>
2011-10-12 18:42:35 +0100

    anzu: Use mogami-common as a base
    This patch uses mogami-common as a base for building the device
    which hopefully will prevent duplication on mogami-based phones.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,23 +1,7 @@
--include device/semc/msm7x30-common/BoardConfigCommon.mk
+-include device/semc/mogami-common/BoardConfigCommon.mk
 
-TARGET_BOOTLOADER_BOARD_NAME := mogami
 TARGET_SPECIFIC_HEADER_PATH := device/semc/anzu/include
-
-BOARD_WPA_SUPPLICANT_DRIVER := CUSTOM
-BOARD_WLAN_DEVICE := wl1271
-BOARD_SOFTAP_DEVICE := wl1271
-WPA_SUPPLICANT_VERSION := VER_0_6_X
-WIFI_DRIVER_MODULE_PATH := /system/lib/modules/tiwlan_drv.ko
-WIFI_DRIVER_MODULE_NAME := tiwlan_drv
-WIFI_FIRMWARE_LOADER := wlan_loader
-WIFI_EXT_MODULE_PATH := /system/lib/modules/sdio.ko
-WIFI_EXT_MODULE_NAME := sdio
 
 HDMI_DUAL_DISPLAY := true
 
 TARGET_OTA_ASSERT_DEVICE := LT18i,LT18a,LT15i,LT15a,anzu
-
-BOARD_OVERLAY_FORMAT_YCrCb_420_SP := true
-BOARD_CAMERA_USE_GETBUFFERINFO := true
-
--include device/semc/msm7x30-common/Android.mk
