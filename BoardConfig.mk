630967283169ed2a6daf1007d0399f61b4fef250
freexperia <jerpelea@gmail.com>
2012-02-25 12:22:04 +0200

    semc: moved usb DevID / device

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,12 +1,10 @@
 -include device/semc/mogami-common/BoardConfigCommon.mk
 -include vendor/semc/anzu/BoardConfigVendor.mk
 
-BUILD SEMC SENSORS := false
-
+BUILD_SEMC_SENSORS := false
 SENSORS_COMPASS_AK897X := true
 SENSORS_ACCEL_BMA150_INPUT := true
 SENSORS_ACCEL_BMA250_INPUT := false
-SENSORS_ACCEL_BMA250NA_INPUT := false
 SENSORS_PROXIMITY_APDS970X := true
 SENSORS_PRESSURE_BMP180 := false
 
--- device_anzu.mk
+++ device_anzu.mk
@@ -28,13 +28,13 @@ PRODUCT_COPY_FILES += \
 
 # Init files
 PRODUCT_COPY_FILES += \
+    device/semc/anzu/prebuilt/init.semc.usb.rc:root/init.semc.usb.rc \
     device/semc/anzu/prebuilt/pre_hw_config.sh:root/pre_hw_config.sh \
     device/semc/anzu/prebuilt/hw_config.sh:system/etc/hw_config.sh \
     device/semc/msm7x30-common/prebuilt/logo_H.rle:root/logo.rle \
     device/semc/anzu/prebuilt/bootrec:root/sbin/bootrec \
     device/semc/anzu/prebuilt/filler:root/sbin/filler \
     device/semc/anzu/recovery.fstab:root/recovery.fstab
-
 
 # semc msm7x30 uses high-density artwork where available
 PRODUCT_LOCALES += hdpi
