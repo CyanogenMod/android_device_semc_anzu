b1e9caec2c45274ffd2aefcc6bf7d594535ae24e
freexperia <jerpelea@gmail.com>
2012-03-01 21:06:52 +0200

    semc: sensor config
    Change-Id: I065e254fa7a8215f9093a35cf72465d30178ff1a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,15 @@
 -include device/semc/mogami-common/BoardConfigCommon.mk
 -include vendor/semc/anzu/BoardConfigVendor.mk
 
+TARGET_SPECIFIC_HEADER_PATH := device/semc/anzu/include
+
+SENSORS_COMPASS_AK897X := true
+SENSORS_ACCEL_BMA150_INPUT := true
+SENSORS_ACCEL_BMA250_INPUT := false
+SENSORS_PROXIMITY_APDS970X := true
+SENSORS_PRESSURE_BMP180 := false
+
+HDMI_DUAL_DISPLAY := true
+TARGET_HAVE_HDMI_OUT := true
+
 TARGET_OTA_ASSERT_DEVICE := LT18i,LT18a,LT15i,LT15a,anzu
