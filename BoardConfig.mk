30e354dbe3a176755304f3d313bfb21164dd1d3f
freexperia <jerpelea@gmail.com>
2012-03-02 04:07:33 +0200

    semc: sensor cleanup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,7 +1,6 @@
 -include device/semc/mogami-common/BoardConfigCommon.mk
 -include vendor/semc/anzu/BoardConfigVendor.mk
 
-BUILD_SEMC_SENSORS := true
 SENSORS_COMPASS_AK897X := true
 SENSORS_ACCEL_BMA150_INPUT := true
 SENSORS_ACCEL_BMA250_INPUT := false
