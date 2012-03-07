a84b295146fb72d9794aa7aca7faa027e511378f
freexperia <jerpelea@gmail.com>
2012-03-07 21:17:04 +0200

    semc: remove duplicate device include
    Change-Id: I823306dc8abdec93ebd3c4a3299da22c55448a3b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,8 +1,6 @@
 -include device/semc/mogami-common/BoardConfigCommon.mk
 -include vendor/semc/anzu/BoardConfigVendor.mk
 
-TARGET_SPECIFIC_HEADER_PATH := device/semc/anzu/include
-
 SENSORS_COMPASS_AK897X := true
 SENSORS_ACCEL_BMA150_INPUT := true
 SENSORS_ACCEL_BMA250_INPUT := false
