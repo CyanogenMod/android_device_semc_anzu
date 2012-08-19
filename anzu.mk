7ab954a28a04a122ec6c21fbb9ca340394d1b074
freexperia <jerpelea@gmail.com>
2012-08-19 10:24:48 +0300

    anzu: add device configs

--- anzu.mk
+++ anzu.mk
@@ -23,6 +23,18 @@ PRODUCT_COPY_FILES += \
     device/semc/anzu/prebuilt/bootrec-device:root/sbin/bootrec-device \
     device/semc/anzu/recovery.fstab:root/recovery.fstab
 
+# Device specific configs
+PRODUCT_COPY_FILES += \
+    device/semc/anzu/config/cy8ctma300_touch.idc:system/usr/idc/cy8ctma300_touch.idc \
+    device/semc/anzu/config/atdaemon.kl:system/usr/keylayout/atdaemon.kl \
+    device/semc/anzu/config/msm_pmic_pwr_key.kl:system/usr/keylayout/msm_pmic_pwr_key.kl \
+    device/semc/anzu/config/pm8058-keypad.kl:system/usr/keylayout/pm8058-keypad.kl \
+    device/semc/anzu/config/simple_remote.kl:system/usr/keylayout/simple_remote.kl \
+    device/semc/anzu/config/als_curve.conf:system/etc/als_curve.conf \
+    device/semc/anzu/config/init.qcom.bt.sh:system/etc/init.qcom.bt.sh \
+    device/semc/anzu/config/sensors.conf:system/etc/sensors.conf \
+    device/semc/anzu/config/tiwlan.ini:system/etc/tiwlan.ini
+
 $(call inherit-product, device/semc/msm7x30-common/prebuilt/resources-hdpi.mk)
 
 #LCD DENSITY
