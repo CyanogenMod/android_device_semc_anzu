1babafd5cd7ddb280068aa23ec0b1315b3f15183
Michael Bestas <mikeioannina@gmail.com>
2013-04-04 23:13:48 +0300

    anzu: Remove obsolete ALS configuration
    Also set button_backlight current on pre_hw_config.sh to avoid too bright recovery indication.
    
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I2a92ac59b573a6f48e52425d196702fb78cdb7c1

--- prebuilt/pre_hw_config.sh
+++ prebuilt/pre_hw_config.sh
@@ -1,6 +1,6 @@
 # LMU AS3676 Configuration  before chargemon
 dev=/sys/devices/i2c-0/0-0040/leds
-echo 0 > $dev/lcd-backlight/als/enable  #Sensor on/off. 1 = on, reg 90h
+echo 450 > $dev/button-backlight/max_current
 echo 20000 > $dev/lcd-backlight/max_current
 echo 1000 > $dev/red/max_current
 echo 1000 > $dev/green/max_current
