5d597b543950f1d1b522620adacd7e79d3b3d971
Michael Bestas <mikeioannina@gmail.com>
2013-04-05 19:15:41 +0300

    anzu: Fix leds & backlight configuration on boot
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: Ib3aa53b9d692fe7e716a36a242c33ce5cb65473d

--- anzu.mk
+++ anzu.mk
@@ -13,7 +13,6 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 # Init files
 PRODUCT_COPY_FILES += \
     device/semc/anzu/prebuilt/init.semc.usb.rc:root/init.semc.usb.rc \
-    device/semc/anzu/prebuilt/pre_hw_config.sh:root/pre_hw_config.sh \
     device/semc/anzu/prebuilt/hw_config.sh:system/etc/hw_config.sh \
     device/semc/msm7x30-common/prebuilt/logo_H.rle:root/logo.rle
 
--- prebuilt/pre_hw_config.sh
+++
@@ -1,7 +0,0 @@
-# LMU AS3676 Configuration  before chargemon
-dev=/sys/devices/i2c-0/0-0040/leds
-echo 450 > $dev/button-backlight/max_current
-echo 20000 > $dev/lcd-backlight/max_current
-echo 1000 > $dev/red/max_current
-echo 1000 > $dev/green/max_current
-echo 1000 > $dev/blue/max_current
